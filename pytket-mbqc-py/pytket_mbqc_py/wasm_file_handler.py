4fea7cbf043666f8d0f9698b0997bb336d8cee31
Daniel Mills <daniel.mills@c2021dqqnf.home>
2024-03-04 14:53:18 +0000

    Add relative wasm import

--- pytket-mbqc-py/pytket_mbqc_py/wasm_file_handler.py
+++ pytket-mbqc-py/pytket_mbqc_py/wasm_file_handler.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 from pytket import wasm
+import importlib_resources
 
 def get_wasm_file_handler():
-    rus_dir = Path().cwd().joinpath('../pytket-mbqc-rus')
-    wasm_file = rus_dir.joinpath('target/wasm32-unknown-unknown/release/pytket_mbqc_rus.wasm')
-    return wasm.WasmFileHandler(wasm_file)
+    return wasm.WasmFileHandler(
+        importlib_resources.files("pytket_mbqc_py").joinpath('pytket_mbqc_rus.wasm')
+    )

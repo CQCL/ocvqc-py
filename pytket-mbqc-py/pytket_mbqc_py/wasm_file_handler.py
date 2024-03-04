7034282f5de3073430c6f4a371ce59b5245ad140
Daniel Mills <daniel.mills@c2021dqqnf.home>
2024-03-04 15:08:13 +0000

    Reformat for ruff

--- pytket-mbqc-py/pytket_mbqc_py/wasm_file_handler.py
+++ pytket-mbqc-py/pytket_mbqc_py/wasm_file_handler.py
@@ -1,8 +1,8 @@
-from pathlib import Path
 from pytket import wasm
 import importlib_resources
 
+
 def get_wasm_file_handler():
     return wasm.WasmFileHandler(
-        importlib_resources.files("pytket_mbqc_py").joinpath('pytket_mbqc_rus.wasm')
+        importlib_resources.files("pytket_mbqc_py").joinpath("pytket_mbqc_rus.wasm")
     )

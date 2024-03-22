29c38d826c5033e313791430a164e877a9b56638
Jonathan Miller <jonathan.miller@fieldofnodes.com>
2024-03-22 15:03:50 +0000

    Remove Python environment from version control

--- pytket-mbqc-env/lib/python3.11/site-packages/pytket_mbqc_py/wasm_file_handler.py
+++
@@ -1,8 +0,0 @@
-from pytket import wasm
-import importlib_resources
-
-
-def get_wasm_file_handler():
-    return wasm.WasmFileHandler(
-        importlib_resources.files("pytket_mbqc_py").joinpath("pytket_mbqc_rus.wasm")
-    )

3a7204332ddbd2766da8a3206a4b86cf889065a0
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-05-02 19:07:39 +0100

    Add docs to qubit manager

--- pytket-mbqc-py/pytket_mbqc_py/qubit_manager.py
+++ pytket-mbqc-py/pytket_mbqc_py/qubit_manager.py
@@ -1,9 +1,41 @@
+"""
+Utilities for managing qubits.
+In particular for measuring and reusing qubits.
+"""
 from pytket import Circuit
 from pytket.unit_id import BitRegister, Qubit
 
+from typing import List, Dict
+
 
 class QubitManager(Circuit):
+    """
+    Manages a collection of qubits. In particular maintains
+    a list of qubits which are not in use. This can be added
+    to by measuring qubits (making them available again)
+    and drawn from by initialising qubits. This is a child of
+    :py:class:`~pytket.circuit.Circuit`.
+
+    :ivar qubit_list: List of available quits.
+    :ivar qubit_initialised: The qubits which have been added
+        to the circuit. Qubits which are never required
+        are never added to the circuit.
+    :ivar qubit_meas_reg: A dictionary mapping qubits to the
+        classical registers where their measurement results
+        will be stored.
+    """
+
+    qubit_list: List[Qubit]
+    qubit_initialised: Dict[Qubit, bool]
+    qubit_meas_reg: Dict[Qubit, BitRegister]
+
     def __init__(self, n_physical_qubits: int) -> None:
+        """Initialisation method. Creates tools for
+        tracking the given number of qubits.
+
+        :param n_physical_qubits: The number of qubits
+            to manage.
+        """
         self.qubit_list = [Qubit(index=i) for i in range(n_physical_qubits)]
         self.qubit_initialised = {qubit: False for qubit in self.qubit_list}
         self.qubit_meas_reg = {
@@ -17,6 +49,12 @@ class QubitManager(Circuit):
             self.add_c_register(register=meas_reg)
 
     def get_qubit(self) -> Qubit:
+        """Return a qubit which is not in use, and which
+        is included in the underlying circuit.
+
+        :raises Exception: Raised if there are no more available qubits.
+        :return: A qubit in the 0 computational basis state.
+        """
         if len(self.qubit_list) == 0:
             raise Exception("You have run out of qubits.")
 
@@ -31,7 +69,9 @@ class QubitManager(Circuit):
         return qubit
 
     @property
-    def physical_qubits_used(self):
+    def physical_qubits_used(self) -> List[Qubit]:
+        """The physical qubits which have been used.
+        """
         return [
             qubit
             for qubit, initialised in self.qubit_initialised.items()
@@ -39,5 +79,12 @@ class QubitManager(Circuit):
         ]
 
     def managed_measure(self, qubit: Qubit) -> None:
+        """Measure the given qubit, storing the result in the
+        qubit's classical register. This will return the qubit to
+        the list of available qubits.
+
+        :param qubit: The qubit to be measured.
+        :type qubit: Qubit
+        """
         self.qubit_list.insert(0, qubit)
         self.Measure(qubit=qubit, bit=self.qubit_meas_reg[qubit][0])
---
+++ pytket-mbqc-py/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pytket-mbqc'
+copyright = '2024, Daniel Mills'
+author = 'Daniel Mills'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    "pytket": ("https://tket.quantinuum.com/api-docs/", None),
+    "python": ("https://docs.python.org/3", None),
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']

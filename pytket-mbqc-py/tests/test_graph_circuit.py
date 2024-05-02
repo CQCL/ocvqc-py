883eface3dafe3b0115d765849fdf04d117e72f7
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-05-02 15:37:20 +0100

    feat: Delay Z correction calculation until measure (#26)
    * Delay z correction calculation
    
    * Move corrections registers to graph state
    
    * Add cnot test with earlier measure
    
    * Additional documentation

--- pytket-mbqc-py/tests/test_graph_circuit.py
+++ pytket-mbqc-py/tests/test_graph_circuit.py
@@ -200,3 +200,117 @@ def test_large_cnot_block():
         n_shots=n_shots,
     )
     assert result.get_counts(output_reg)[output_state] == n_shots
+
+
+def test_3_q_ghz():
+    graph_circuit = GraphCircuit(n_physical_qubits=5)
+
+    input_quibt, input_vertex = graph_circuit.add_input_vertex()
+
+    graph_circuit.H(input_quibt)
+
+    vertex_layer_1_1 = graph_circuit.add_graph_vertex()
+    vertex_layer_1_2 = graph_circuit.add_graph_vertex()
+
+    graph_circuit.add_edge(input_vertex, vertex_layer_1_1)
+    graph_circuit.add_edge(input_vertex, vertex_layer_1_2)
+
+    graph_circuit.corrected_measure(vertex=input_vertex)
+
+    vertex_layer_2_1 = graph_circuit.add_graph_vertex()
+    vertex_layer_2_2 = graph_circuit.add_graph_vertex()
+
+    graph_circuit.add_edge(vertex_layer_1_1, vertex_layer_2_1)
+    graph_circuit.add_edge(vertex_layer_1_1, vertex_layer_2_2)
+
+    graph_circuit.corrected_measure(vertex=vertex_layer_1_1)
+
+    output_qubits = graph_circuit.get_outputs()
+
+    graph_circuit.H(output_qubits[3])
+    graph_circuit.H(output_qubits[4])
+
+    graph_circuit.CX(output_qubits[2], output_qubits[3])
+    graph_circuit.CX(output_qubits[2], output_qubits[4])
+    graph_circuit.H(output_qubits[2])
+
+    output_c_reg = graph_circuit.add_c_register(
+        name="output measure reg", size=len(output_qubits)
+    )
+    for qubit, bit in zip(output_qubits.values(), output_c_reg):
+        graph_circuit.Measure(qubit=qubit, bit=bit)
+
+    api_offline = QuantinuumAPIOffline()
+    backend = QuantinuumBackend(device_name="H1-1LE", api_handler=api_offline)
+    compiled_graph_circuit = backend.get_compiled_circuit(
+        circuit=graph_circuit, optimisation_level=0
+    )
+    n_shots = 100
+    result = backend.run_circuit(circuit=compiled_graph_circuit, n_shots=n_shots)
+    assert result.get_counts(cbits=output_c_reg)[(0, 0, 0)] == n_shots
+
+
+@pytest.mark.parametrize(
+    "input_state, output_state",
+    [((0, 0), (0, 0)), ((0, 1), (0, 1)), ((1, 0), (1, 1)), ((1, 1), (1, 0))],
+)
+def test_cnot_early_measure(input_state, output_state):
+    circuit = GraphCircuit(n_physical_qubits=3)
+
+    target_qubit, vertex_one = circuit.add_input_vertex()
+    if input_state[1]:
+        circuit.X(target_qubit)
+
+    vertex_two = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_one, vertex_two=vertex_two)
+
+    circuit.corrected_measure(vertex=vertex_one, t_multiple=0)
+
+    control_qubit, vertex_three = circuit.add_input_vertex()
+    if input_state[0]:
+        circuit.X(control_qubit)
+
+    vertex_four = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_two, vertex_two=vertex_four)
+
+    circuit.corrected_measure(vertex=vertex_two, t_multiple=0)
+
+    vertex_five = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_three, vertex_two=vertex_five)
+
+    circuit.corrected_measure(vertex=vertex_three, t_multiple=0)
+
+    vertex_six = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_four, vertex_two=vertex_six)
+
+    circuit.corrected_measure(vertex=vertex_four, t_multiple=0)
+
+    vertex_seven = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_five, vertex_two=vertex_seven)
+
+    circuit.corrected_measure(vertex=vertex_five, t_multiple=0)
+
+    vertex_eight = circuit.add_graph_vertex()
+    circuit.add_edge(vertex_one=vertex_six, vertex_two=vertex_eight)
+
+    circuit.add_edge(vertex_one=vertex_six, vertex_two=vertex_seven)
+
+    circuit.corrected_measure(vertex=vertex_six, t_multiple=0)
+
+    output_qubits = circuit.get_outputs()
+    output_reg = BitRegister(name="output", size=2)
+    circuit.add_c_register(register=output_reg)
+    circuit.Measure(qubit=output_qubits[vertex_seven], bit=output_reg[0])
+    circuit.Measure(qubit=output_qubits[vertex_eight], bit=output_reg[1])
+
+    api_offline = QuantinuumAPIOffline()
+    backend = QuantinuumBackend(device_name="H1-1LE", api_handler=api_offline)
+    compiled_circuit = backend.get_compiled_circuit(circuit)
+
+    n_shots = 100
+    result = backend.run_circuit(
+        circuit=compiled_circuit,
+        n_shots=n_shots,
+    )
+
+    assert result.get_counts(output_reg)[output_state] == n_shots

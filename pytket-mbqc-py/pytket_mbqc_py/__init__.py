617f274842bd50742c1edb56aaf27a55439c5497
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-08-27 17:00:18 +0100

    Add two qubit grover (#99)

--- pytket-mbqc-py/pytket_mbqc_py/__init__.py
+++ pytket-mbqc-py/pytket_mbqc_py/__init__.py
@@ -6,3 +6,4 @@ from .cnot_grid import CNOTBlock
 from .graph_circuit import GraphCircuit
 from .qubit_manager import QubitManager
 from .random_graph import RandomIdentityGraph
+from .two_qubit_grover import TwoQubitGrover

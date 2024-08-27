dad1adba897afd44a2dbab286fb2771916d9833c
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-08-27 15:34:27 +0100

    Add two qubit grover

---
+++ pytket-mbqc-py/pytket_mbqc_py/two_qubit_grover.py
@@ -0,0 +1,80 @@
+"""
+Module implementing the 2 qubit grover example from:
+https://journals.aps.org/pra/abstract/10.1103/PhysRevA.104.062422.
+"""
+
+from pytket_mbqc_py import GraphCircuit
+
+
+class TwoQubitGrover(GraphCircuit):
+    """Class implementing the 2 qubit grover example from:
+    https://journals.aps.org/pra/abstract/10.1103/PhysRevA.104.062422.
+    The marked value is returned with probability 1.
+    """
+
+    def __init__(self, tau: int) -> None:
+        """Initialisation method.
+
+        :param tau: Marked value.
+        :type tau: int
+        :raises Exception: Raised if the marked value is not one of
+            0, 1, 2, or 3.
+        """
+        match tau:
+            case 0:
+                theta_three = 4
+                theta_four = 4
+            case 1:
+                theta_three = 4
+                theta_four = 0
+            case 2:
+                theta_three = 0
+                theta_four = 4
+            case 3:
+                theta_three = 0
+                theta_four = 0
+            case _:
+                raise Exception("tau must be one of 0, 1, 2 or 3.")
+
+        super().__init__(
+            n_physical_qubits=3,
+            n_logical_qubits=8,
+            vertex_is_dummy_list=[
+                [True, False, False, True, False, True, False, True],
+                [False, True, True, False, True, False, True, False],
+            ],
+        )
+
+        vertex_one = self.add_graph_vertex(measurement_order=0)
+
+        vertex_two = self.add_graph_vertex(measurement_order=1)
+
+        vertex_three = self.add_graph_vertex(measurement_order=3)
+        self.add_edge(vertex_one=vertex_one, vertex_two=vertex_three)
+        self.add_edge(vertex_one=vertex_one, vertex_two=vertex_two)
+        self.corrected_measure(vertex=vertex_one)
+
+        vertex_four = self.add_graph_vertex(measurement_order=2)
+        self.add_edge(vertex_one=vertex_two, vertex_two=vertex_four)
+        self.corrected_measure(vertex=vertex_two)
+
+        vertex_five = self.add_graph_vertex(measurement_order=5)
+        self.add_edge(vertex_one=vertex_four, vertex_two=vertex_five)
+        self.corrected_measure(vertex=vertex_four, t_multiple=theta_four)
+
+        vertex_six = self.add_graph_vertex(measurement_order=4)
+        self.add_edge(vertex_one=vertex_three, vertex_two=vertex_six)
+        self.corrected_measure(vertex=vertex_three, t_multiple=theta_three)
+
+        vertex_seven = self.add_graph_vertex(measurement_order=None)
+        self.add_edge(vertex_one=vertex_six, vertex_two=vertex_seven)
+        self.corrected_measure(vertex=vertex_six)
+
+        vertex_eight = self.add_graph_vertex(measurement_order=None)
+        self.add_edge(vertex_one=vertex_five, vertex_two=vertex_eight)
+        self.corrected_measure(vertex=vertex_five)
+
+        self.add_edge(vertex_one=vertex_eight, vertex_two=vertex_seven)
+
+        self.corrected_measure(vertex=vertex_seven, t_multiple=4)
+        self.corrected_measure(vertex=vertex_eight, t_multiple=4)
---
+++ pytket-mbqc-py/tests/test_two_qubit_grover.py
@@ -0,0 +1,37 @@
+import pytest
+from pytket.extensions.quantinuum import QuantinuumAPIOffline, QuantinuumBackend
+
+from pytket_mbqc_py import TwoQubitGrover
+
+
+@pytest.mark.parametrize(
+    "tau, ideal_output",
+    [
+        (0, (0, 0)),
+        (1, (0, 1)),
+        (2, (1, 0)),
+        (3, (1, 1)),
+    ],
+)
+def test_cnot_grid(tau, ideal_output):
+    graph_circuit = TwoQubitGrover(tau=tau)
+
+    backend = QuantinuumBackend(
+        device_name="H1-1LE",
+        api_handler=QuantinuumAPIOffline(),
+    )
+
+    compiled_graph_circuit = backend.get_compiled_circuit(circuit=graph_circuit)
+
+    n_shots = 100
+    result = backend.run_circuit(circuit=compiled_graph_circuit, n_shots=n_shots)
+    counts = graph_circuit.get_output_result(result=result).get_counts()
+    assert counts[ideal_output] == counts.total()
+
+
+def test_cnot_grid_error():
+    with pytest.raises(
+        Exception,
+        match="tau must be one of 0, 1, 2 or 3.",
+    ):
+        TwoQubitGrover(tau=4)

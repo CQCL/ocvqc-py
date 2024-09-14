33a352845cd51147e85624b9718b7017cf6c031c
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-09-14 15:17:53 +0100

    Add decomposition tests

--- pytket-mbqc-py/tests/test_cnot_grid.py
+++ pytket-mbqc-py/tests/test_cnot_grid.py
@@ -66,8 +66,8 @@ def test_cnot_grid_verified(input_string, n_layers, output_state):
         api_handler=QuantinuumAPIOffline(),
     )
 
-    # DecomposeClassicalExp().apply(graph_circuit)
     compiled_graph_circuit = backend.get_compiled_circuit(circuit=graph_circuit)
+    DecomposeClassicalExp().apply(compiled_graph_circuit)
 
     n_shots = 100
     result = backend.run_circuit(circuit=compiled_graph_circuit, n_shots=n_shots)
